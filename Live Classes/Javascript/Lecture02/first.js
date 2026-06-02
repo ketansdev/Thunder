40e7c6727c09c8e06216dfcdb8acf481fe8351aa
Ketan Shetge <ketanshetgedev@gmail.com>
2026-06-02 23:01:29 +0530

    add Thunder - Lecture02

---
+++ Live Classes/Javascript/Lecture02/first.js
@@ -0,0 +1,127 @@
+// console.log("Hello World");
+
+// let a = 10;
+// console.log(a);
+
+
+// let firstString = "Ketan Shetge";
+// console.log(firstString);
+
+
+// const a = 30;
+// console.log(a);
+
+
+// const a = "Ketan Shetge";
+// console.log(a);
+
+
+// const a = 30.5;
+// a = 7
+// console.log(a);
+
+
+// let a = 30;
+// let b = a;
+// b = 70;
+
+// console.log(a, b);
+
+
+
+// Primitive Data types and Non Primitive data types
+
+// Primitive Data types : Number, string , boolean , undefined , null, symbol, bigInt
+// immutable
+
+// let firstNumber = 20;
+// let secondNumber = 30.5;
+// let age = 25;
+
+// console.log(firstNumber, secondNumber);
+
+
+// string
+
+// let firstString = `Ketan Shetge and age is ${age}`;
+// let secondString = "Yash Shinde";
+// let thirdString = `Ketan is Smart`;
+
+// console.log(firstString, secondString, thirdString);
+
+
+// boolean
+
+// let firstBoolean = true;
+// let secondBoolean = false;
+
+// console.log(firstBoolean, secondBoolean);
+
+
+// undefined
+
+// let a;
+// console.log(a);
+
+
+// null
+
+// let b = null;
+// console.log(b);
+
+
+// bigint
+
+// let a = 243263673246983923643267432n;
+// console.log(a);
+
+
+// symbol
+
+// let a = Symbol("Ketan");
+// console.log(a);
+// let b = Symbol("Ketan");
+// console.log(b);
+
+// console.log(a === b);
+
+
+
+// Non Primitive Data types : Object, Array, Functions
+
+// array
+// let arr = [10, 20, 30, "Ketan", true, 10.5]
+// console.log(arr);
+
+
+// object
+
+// let person = {
+//     name : "Ketan",
+//     age : 25,
+//     city: "Mumbai"
+// }
+
+// console.log(person);
+
+
+// function
+
+// function greet(){
+//     console.log("Hello Ketan");
+// }
+
+// greet();
+
+// let a = function greet(){
+//     console.log("Hello Ketan");
+// }
+
+// console.log(a);
+
+
+let a = function(){
+    console.log("Hello");
+}
+
+a();
