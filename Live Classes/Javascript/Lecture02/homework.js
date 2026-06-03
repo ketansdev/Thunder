3829a03e396d02df58eb80781d6bc5fd587d2140
Ketan Shetge <ketanshetgedev@gmail.com>
2026-06-03 21:30:08 +0530

    output type added for each data type

--- Live Classes/Javascript/Lecture02/homework.js
+++ Live Classes/Javascript/Lecture02/homework.js
@@ -2,78 +2,77 @@
 
 //  number
 // let a = 10;
-// console.log(typeof a)
+// console.log(typeof a) //number
 
 // float --> number
 // let b = 20.5;
-// console.log(typeof b)
+// console.log(typeof b) // number
 
 // string
 // let firstString = "Ketan Shetge"
-// console.log(typeof firstString)
+// console.log(typeof firstString) //string
 
 
 // boolean
 // let firstBoolean = true;
-// console.log(typeof firstBoolean)
+// console.log(typeof firstBoolean) //boolean
 
 
 
 // // undefined
 // let a;
-// console.log(typeof a)
+// console.log(typeof a) //undefined
 
 
 // null
 // let b = null;
-// console.log(typeof b);
-
+// console.log(typeof b);  // object
 
 
 // bigint
 // let a = 243263673246983923643267432n;
-// console.log(typeof a)
+// console.log(typeof a) // bigint
 
 
 // symbol
 // let a = Symbol("Ketan");
-// console.log(typeof a);
+// console.log(typeof a);  // symbol
 
 
 // Non Primitive Data types
 
 // array
-let arr = [10, 20, 30, "Ketan", true, 10.5]
+// let arr = [10, 20, 30, "Ketan", true, 10.5]
 
-console.log(typeof arr);
+// console.log(typeof arr);  // object
 
 
 // object
 
-let person = {
-    name : "Ketan",
-    age : 25,
-    city: "Mumbai"
-}
+// let person = {
+//     name : "Ketan",
+//     age : 25,
+//     city: "Mumbai"
+// }
 
-console.log(typeof person);
+// console.log(typeof person); // object
 
 
 // function
 
-function greet(){
-    console.log("Hello Ketan");
-}
+// function greet(){
+//     console.log("Hello Ketan");
+// }
 
-greet();
+// greet();
 
-console.log(typeof greet)
+// console.log(typeof greet) // function
 
 
-let a = function(){
-    console.log("Hello");
-}
+// let a = function(){
+//     console.log("Hello");
+// }
 
-a();
+// a();
 
-console.log(typeof a)
+// console.log(typeof a) // function
